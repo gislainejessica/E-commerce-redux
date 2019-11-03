d364cb7c49c3e24cad9b4ffcb12ebfd91ce3ce79
GislaineJessica <GislainehJessica@gmail.com>
2019-11-03 09:50:15 -0300

    Estruturando as rotas

--- rocketshoes/src/App.js
+++ rocketshoes/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './routes'
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
-    <div className="App">
-      <h1> RocketShoes </h1>
-    </div>
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
   );
 }
 
