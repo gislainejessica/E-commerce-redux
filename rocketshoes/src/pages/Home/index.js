3d8f4c83affb390df16b6c9a5a77ffa204d53383
GislaineJessica <GislainehJessica@gmail.com>
2019-11-03 18:13:37 -0300

    conexao com api json-server

--- rocketshoes/src/pages/Home/index.js
+++ rocketshoes/src/pages/Home/index.js
@@ -1,82 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
-import { ProductList } from './styles';
+import { ProductList } from './styles'
+import api from '../../services/api'
+import { formatPrice } from '../../util/format'
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
-        <strong> Tênis muito legal </strong>
-        <span>R$ 2000,00</span>
+export default class Home extends Component {
+  state = {
+    products: []
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 1
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
-        <strong> Tênis muito legal </strong>
-        <span>R$ 2000,00</span>
+  async componentDidMount(){
+    const response = await api.get('products')
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }))
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
-        <strong> Tênis muito legal </strong>
-        <span>R$ 2000,00</span>
+    this.setState({products: data})
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 4
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
-        <strong> Tênis muito legal </strong>
-        <span>R$ 2000,00</span>
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 2
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
-        <strong> Tênis muito legal </strong>
-        <span>R$ 2000,00</span>
+  render(){
+    const { products } = this.state
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={ product.id }>
+          <img src={ product.image } alt={product.title}/>
+          <strong> { product.title } </strong>
+          <span> { product.priceFormatted } </span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
-        <strong> Tênis muito legal </strong>
-        <span>R$ 2000,00</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 1
+            </div>
+            <span> ADICIONAR AO CARRINHO </span>
+          </button>
+        </li>
+        ))}
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-    </ProductList>
-  );
+      </ProductList>
+    );
+  }
 }
