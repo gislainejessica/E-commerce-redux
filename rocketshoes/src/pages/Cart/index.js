6a1a9c2b9f8440a329a66d239e0e194cdbb25450
GislaineJessica <GislainehJessica@gmail.com>
2019-11-03 15:18:10 -0300

    cart styles and api

--- rocketshoes/src/pages/Cart/index.js
+++ rocketshoes/src/pages/Cart/index.js
@@ -1,9 +1,63 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <h1> Cart </h1>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th> PRODUTO </th>
+            <th> QTD </th>
+            <th> SUBTOTAL </th>
+            <th/>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="tenis"/>
+            </td>
+            <td>
+              <strong>Ténis muito massa</strong>
+              <span> R$ 122,77 </span>
+            </td>
+            <td>
+              <div>
+              <button>
+                <MdRemoveCircleOutline size={20} color="#7159c1" />
+              </button>
+              <input type="number" readOnly value={2}/>
+              <button>
+                <MdAddCircleOutline size={20} color="#7159c1" />
+              </button>
+              </div>
+              </td>
+              <td>
+                <strong>R$ 245,54</strong>
+              </td>
+              <td>
+                <button type="button">
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+
+
+            </td>
+          </tr>
+        </tbody>
+
+      </ProductTable>
+      <footer>
+        <button type='button'>
+          Finalizar Pedido
+        </button>
+        <Total>
+            <span> TOTAL </span>
+            <strong> R$ 5000,00 </strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
