add1227b2a909011607aefe17808301b98a8304a
GislaineJessica <GislainehJessica@gmail.com>
2019-11-03 12:11:51 -0300

    Home styles

--- rocketshoes/src/pages/Home/index.js
+++ rocketshoes/src/pages/Home/index.js
@@ -1,9 +1,82 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <h1> Home </h1>
+    <ProductList>
+      <li>
+        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
+        <strong> Tênis muito legal </strong>
+        <span>R$ 2000,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 1
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
+        <strong> Tênis muito legal </strong>
+        <span>R$ 2000,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
+        <strong> Tênis muito legal </strong>
+        <span>R$ 2000,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 4
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
+        <strong> Tênis muito legal </strong>
+        <span>R$ 2000,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 2
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
+        <strong> Tênis muito legal </strong>
+        <span>R$ 2000,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/4a5e79f062cd44bb9c64a93f01221341_9366/Tenis_Ultraboost_Branco_F36155_01_standard.jpg" alt="chuteira"/>
+        <strong> Tênis muito legal </strong>
+        <span>R$ 2000,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
