ff25a35394c447274b3914bf5fe4dd3563438ee4
GislaineJessica <GislainehJessica@gmail.com>
2019-11-05 06:37:32 -0300

    Add Estoque

--- rocketshoes/src/store/modules/cart/sagas.js
+++ rocketshoes/src/store/modules/cart/sagas.js
@@ -7,9 +7,18 @@ function* addToCart({ id }){
   const productExists = yield select(
     state => state.cart.find(p => p.id ===id)
   )
+  const stock = yield call(api.get, `/stock/${id}`)
+  const stockAmount = stock.data.amount
+  const currentAmount = productExists ? productExists.amount : 0
+  const amount = currentAmount + 1
+
+  if (amount > stockAmount){
+    console.tron.warn('ERRO')
+    return
+  }
+
   if (productExists){
-    const amout = productExists.amount + 1
-    yield put(updateAmount(id, amout))
+    yield put(updateAmount(id, amount))
 
   } else {
     const response = yield call(api.get, `/products/${id}`)
